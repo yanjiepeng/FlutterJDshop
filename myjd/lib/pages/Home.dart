ec5b571437105ffa3869872d16b9ccbea488cff6
yanjiepeng <yan930806@gmail.com>
2019-07-23 15:19:03 +0800

    首页界面

--- myjd/lib/pages/Home.dart
+++ myjd/lib/pages/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:myjd/common/ScreenAdapter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,22 +9,182 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //加载轮播图
+
+  Widget _swiperWidget() {
+    List<Map> imgList = [
+      {"url": "https://www.itying.com/images/flutter/slide01.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide02.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide03.jpg"},
+    ];
+
+    return Container(
+      child: AspectRatio(
+        aspectRatio: 2 / 1,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.network(
+              imgList[index]['url'],
+              fit: BoxFit.cover,
+            );
+          },
+          itemCount: imgList.length,
+          pagination: new SwiperPagination(),
+          autoplay: true,
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget(value) {
+    return Container(
+      height: ScreenUtil.getInstance().setHeight(32),
+      margin: EdgeInsets.only(left: ScreenUtil.getInstance().setWidth(20)),
+      padding: EdgeInsets.only(left: ScreenUtil.getInstance().setWidth(20)),
+      decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+        color: Colors.red,
+        width: ScreenUtil.getInstance().setWidth(10),
+      ))),
+      child: Text(
+        value,
+        style: TextStyle(color: Colors.black54),
+      ),
+    );
+  }
+
+  Widget getGuessLike() {
+    List<Map> guessLike = [
+      {"url": "https://www.itying.com/images/flutter/slide01.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide02.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide03.jpg"},
+    ];
+
+    return Container(
+      height: ScreenUtil.getInstance().setHeight(234),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: guessLike.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    height: ScreenUtil.getInstance().setHeight(140),
+                    width: ScreenUtil.getInstance().setHeight(140),
+                    child: Image.network(
+                      guessLike[index]['url'],
+                      fit: BoxFit.cover,
+                    )),
+                SizedBox(
+                  height: ScreenUtil.getInstance().setHeight(10),
+                ),
+                Text('第$index条')
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  //推荐商品
+
+  _recProductItemWidget() {
+    var itemWidth = (ScreenAdaper.getScreenWidth() - 30) / 2;
+
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: itemWidth,
+      decoration: BoxDecoration(
+          border:
+              Border.all(color: Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: AspectRatio(
+              //防止服务器返回的图片大小不一致导致高度不一致问题
+              aspectRatio: 1 / 1,
+              child: Image.network(
+                "https://www.itying.com/images/flutter/list1.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: ScreenAdaper.height(20)),
+            child: Text(
+              "2019夏季新款气质高贵洋气阔太太有女人味中长款宽松大码",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(color: Colors.black54),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: ScreenAdaper.height(20)),
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "¥188.0",
+                    style: TextStyle(color: Colors.red, fontSize: 16),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("¥198.0",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14,
+                          decoration: TextDecoration.lineThrough)),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getGridview() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Wrap(
+        runSpacing: 10,
+        spacing: 10,
+        children: <Widget>[
+          _recProductItemWidget(),
+          _recProductItemWidget(),
+          _recProductItemWidget(),
+          _recProductItemWidget(),
+          _recProductItemWidget(),
+          _recProductItemWidget(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    //初始化适配工具
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+
+    return ListView(
       children: <Widget>[
-        Text('首页'),
-      
-        RaisedButton(
-            onPressed: (){
-              Navigator.pushNamed(context, '/search');
-            },
-            child: Text('到跳转'),
-
-        )
+        _swiperWidget(),
+        SizedBox(
+          height: 10,
+        ),
+        _titleWidget('猜你喜欢'),
+        getGuessLike(),
+        _titleWidget("热门推荐"),
+        getGridview()
       ],
-
-
     );
   }
 }
