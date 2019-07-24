d06309ebf7b0fa568bd1bce3cc3853d6ce20048e
yanjiepeng <382878591@qq.com>
2019-07-24 17:49:52 +0800

    增加级联菜单数据请求

--- myjd/lib/pages/Category.dart
+++ myjd/lib/pages/Category.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import '../common/ScreenAdapter.dart';
+import 'package:dio/dio.dart';
+import '../config/config.dart';
+
+import '../model/CategoryModel.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -6,8 +11,166 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryeState extends State<CategoryPage> {
+  int _selectIndex = 0;
+
+  List<CategoryItemModel> _leftCategory = [];
+  List<CategoryItemModel> _rightCategory = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getLeftCategory();
+  }
+
+  _getLeftCategory() async {
+    var api = '${Config.domain}api/pcate';
+    var result = await Dio().get(api);
+    var leftList = CategoryModel.fromJson(result.data);
+    print(leftList.result);
+    setState(() {
+      this._leftCategory = leftList.result;
+    });
+    print('pid=${this._leftCategory[0].sId}');
+    _getRightCategory(this._leftCategory[0].sId);
+  }
+
+  _getRightCategory(String pid) async {
+    var api = '${Config.domain}api/pcate?pid=$pid';
+    var result = await Dio().get(api);
+    var rightList = CategoryModel.fromJson(result.data);
+    print(rightList.result);
+    setState(() {
+      this._rightCategory = rightList.result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('分类');
+    ScreenAdaper.init(context);
+
+    var leftWidth = ScreenAdaper.getScreenWidth() / 4;
+    //右侧gridview每一项item的宽度
+    var rightItemWidth =
+        (ScreenAdaper.getScreenWidth() - leftWidth - 20 - 20) / 3;
+    //右侧gridview每一项item的高度
+
+    rightItemWidth = ScreenAdaper.width(rightItemWidth);
+    //转化为屏幕适配的尺寸
+    //高度等于宽度+字体
+    var rightItemHeight = rightItemWidth + ScreenAdaper.height(28);
+
+    return Row(
+      children: <Widget>[
+        _leftWidgt(leftWidth),
+        _rightWidget(rightItemWidth, rightItemHeight)
+      ],
+    );
+  }
+
+  Expanded _rightWidget(rightItemWidth, rightItemHeight) {
+    if (this._rightCategory.length > 0) {
+      return Expanded(
+        flex: 1,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          height: double.infinity,
+          color: Color.fromRGBO(240, 246, 246, 0.9),
+          child: GridView.builder(
+            itemCount: this._rightCategory.length,
+            itemBuilder: (BuildContext context, int index) {
+
+              String pic =  this._rightCategory[index].pic;
+              pic = Config.domain+pic.replaceAll('\\', '/');
+
+              return Container(
+                child: Column(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 1 / 1,
+                      child: Image.network(
+                        pic,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      height: ScreenAdaper.height(28),
+                      child: Text('${this._rightCategory[index].title}'),
+                    )
+                  ],
+                ),
+              );
+            },
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: rightItemWidth / rightItemHeight,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Expanded(
+        flex: 1,
+        child: Container(
+
+          padding: EdgeInsets.all(10),
+          height: double.infinity,
+          color: Color.fromRGBO(240, 246, 246, 0.9),
+          child: Center(
+            child: Text('loading'),
+          )
+        ),
+      );
+    }
+  }
+
+  Container _leftWidgt(leftWidth) {
+    if (this._leftCategory.length > 0) {
+      return Container(
+        width: leftWidth,
+        height: double.infinity,
+        child: ListView.builder(
+          itemCount: this._leftCategory.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      this._selectIndex = index;
+
+                      this._getRightCategory(this._leftCategory[index].sId);
+
+                    });
+                  },
+                  child: Container(
+                    child: Text(
+                      '${this._leftCategory[index].title}',
+                      textAlign: TextAlign.center,
+                    ),
+                    height: ScreenAdaper.height(84),
+                    padding: EdgeInsets.only(top: ScreenAdaper.height(34)),
+                    width: double.infinity,
+                    color: this._selectIndex == index
+                        ? Color.fromRGBO(240, 246, 246, 0.9)
+                        : Colors.white,
+                  ),
+                ),
+                Divider(
+                  height: 1,
+                )
+              ],
+            );
+          },
+        ),
+      );
+    } else {
+      return Container(
+        width: leftWidth,
+        height: double.infinity,
+      );
+    }
   }
 }
