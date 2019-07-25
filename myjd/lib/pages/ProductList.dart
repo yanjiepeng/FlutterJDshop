d2cc6789b3a223ad7852d7d1aef0f3092059b77c
yanjiepeng <yan930806@gmail.com>
2019-07-25 13:52:59 +0800

    商品详情

--- myjd/lib/pages/ProductList.dart
+++ myjd/lib/pages/ProductList.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import '../common/ScreenAdapter.dart';
 
-
+// ignore: must_be_immutable
 class ProductListPage extends StatefulWidget {
-
   Map arguments;
-  
+
   ProductListPage(this.arguments);
 
   @override
@@ -15,31 +14,93 @@ class ProductListPage extends StatefulWidget {
 }
 
 class _ProductListPageState extends State<ProductListPage> {
-
-
-
   @override
   Widget build(BuildContext context) {
-
     ScreenAdaper.init(context);
 
-
     return Scaffold(
-
       appBar: AppBar(
-
         title: Text('商品列表'),
-      
-
       ),
-
-      body:Padding(
-
+      body: Padding(
         padding: EdgeInsets.all(10),
-        child:
-        
-      ),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: <Widget>[
 
+                Row(
+                  children: <Widget>[
+                    
+                    Container(
+                      
+                      width: ScreenAdaper.width(180),
+                      height: ScreenAdaper.height(180),
+                      child: Image.network('https://imgs.aixifan.com/style/image/201907/ccoYfmsvGxCTI9FVUTye8W6N6DLhQefn.jpg' ,fit: BoxFit.cover,),
+                    ),
+                    Expanded(
+                      flex: 1,
+                        child:Container(
+                          width: ScreenAdaper.width(180),
+                          height: ScreenAdaper.height(180),
+                          margin: EdgeInsets.only(left: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                  "戴尔(DELL)灵越3670 英特尔酷睿i5 高性能 台式电脑整机(九代i5-9400 8G 256G)",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    height: ScreenAdaper.height(36),
+                                    margin: EdgeInsets.only(right: 10),
+                                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+
+                                    //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromRGBO(230, 230, 230, 0.9),
+                                    ),
+
+                                    child: Text("4g"),
+                                  ),
+                                  Container(
+                                    height: ScreenAdaper.height(36),
+                                    margin: EdgeInsets.only(right: 10),
+                                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromRGBO(230, 230, 230, 0.9),
+                                    ),
+                                    child: Text("126"),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                "¥990",
+                                style:
+                                TextStyle(color: Colors.red, fontSize: 16),
+                              )
+
+                            ],
+                          ),
+
+                        )
+                    ),
+                    
+                  ],
+                )
+                ,        Divider(height: 20)
+                
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
