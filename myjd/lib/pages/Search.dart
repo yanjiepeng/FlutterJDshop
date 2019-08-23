4286e444aa4e1cb5cecbfcce7968b95b75f45796
yanjiepeng <yan930806@gmail.com>
2019-08-23 16:18:46 +0800

    搜索标题栏

--- myjd/lib/pages/Search.dart
+++ myjd/lib/pages/Search.dart
@@ -7,31 +7,25 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-
   @override
   Widget build(BuildContext context) {
     ScreenAdapter.init(context);
 
-    return  Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Container(
-
             child: TextField(
               autofocus: true,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none
-                ),
+                    borderSide: BorderSide.none),
               ),
-
-
             ),
             height: ScreenAdapter.height(68),
             decoration: BoxDecoration(
                 color: Color.fromRGBO(233, 233, 233, 0.8),
-                borderRadius: BorderRadius.circular(30)
-            ),
+                borderRadius: BorderRadius.circular(30)),
           ),
           actions: <Widget>[
             InkWell(
@@ -39,17 +33,125 @@ class _SearchPageState extends State<SearchPage> {
                 height: ScreenAdapter.height(68),
                 width: ScreenAdapter.width(80),
                 child: Row(
-                  children: <Widget>[
-                    Text('搜索')
-                  ],
+                  children: <Widget>[Text('搜索')],
                 ),
               ),
-
             )
-
           ],
         ),
-        body: Text('搜索')
-    );
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  '热搜',
+                  style: Theme.of(context).textTheme.title,
+                ),
+              ),
+              Divider(),
+              Wrap(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(233, 233, 233, 0.9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('笔记本电脑'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(233, 233, 233, 0.9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('女装'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(233, 233, 233, 0.9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('男装'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(233, 233, 233, 0.9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('测试'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(233, 233, 233, 0.9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('手机'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(233, 233, 233, 0.9),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text('男装'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: Text(
+                  '历史记录',
+                  style: Theme.of(context).textTheme.title,
+                ),
+              ),
+              Divider(),
+              Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text("女装"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("女装"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("男装"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("手机"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("鞋子"),
+                  ),
+                  Divider(),
+                ],
+              ),
+              SizedBox(height: 100),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: ScreenAdapter.width(400),
+                  height: ScreenAdapter.height(64),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black45, width: 1)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[Icon(Icons.delete), Text("清空历史记录")],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- myjd/lib/pages/tabs/Tab.dart
+++ myjd/lib/pages/tabs/Tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myjd/common/ScreenAdapter.dart';
 import 'package:myjd/pages/Category.dart';
 import 'package:myjd/pages/Home.dart';
 import 'package:myjd/pages/Mine.dart';
@@ -10,53 +11,86 @@ class Tabs extends StatefulWidget {
 }
 
 class _TabsState extends State<Tabs> {
-
   int _currentIndex = 0;
   PageController _pageController;
   List<Widget> _pageList = [
-
-      HomePage(),
-      CategoryPage(),
-      ShopCartPage(),
-      MinePage(),
-
+    HomePage(),
+    CategoryPage(),
+    ShopCartPage(),
+    MinePage(),
   ];
 
   @override
   void initState() {
     super.initState();
     this._pageController = PageController(initialPage: this._currentIndex);
-    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    ScreenAdapter.init(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Title'),
+      appBar: this._currentIndex != 3
+          ? AppBar(
+              leading: IconButton(
+                  icon: Icon(
+                    Icons.center_focus_weak,
+                    size: 28,
+                    color: Colors.black87,
+                  ),
+                  onPressed: null),
+              title: InkWell(
+                child: Container(
+                  height: ScreenAdapter.height(68),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromRGBO(233, 233, 233, 0.9)),
+                  padding: EdgeInsets.only(left: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.search),
+                      Text(
+                        '笔记本',
+                        style: TextStyle(fontSize: ScreenAdapter.size(18)),
+                      )
+                    ],
+                  ),
+                ),
+                onTap: (){
+                  Navigator.pushNamed(context, '/search');
+                },
+              ),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.message , size: 28, color: Colors.black87,), onPressed: null)
+              ],
+            )
+          : AppBar(
+        
+        title: Text('企业中心'),
+        
       ),
-      body:PageView(
+      body: PageView(
         controller: this._pageController,
         children: this._pageList,
-
       ),
       bottomNavigationBar: BottomNavigationBar(
-        
-        currentIndex:this._currentIndex  ,
+        currentIndex: this._currentIndex,
         type: BottomNavigationBarType.fixed,
         fixedColor: Colors.red,
-        onTap: (index){
-            setState(() {
-             this._currentIndex = index; 
-             this._pageController.jumpToPage(this._currentIndex);
-            });
+        onTap: (index) {
+          setState(() {
+            this._currentIndex = index;
+            this._pageController.jumpToPage(this._currentIndex);
+          });
         },
-        
         items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home) , title: Text('首页')),
-            BottomNavigationBarItem(icon: Icon(Icons.category)  ,title: Text('分类')),
-            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart)  ,title: Text('购物车')),
-            BottomNavigationBarItem(icon: Icon(Icons.people)  ,title: Text('我的')),
-
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), title: Text('分类')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), title: Text('购物车')),
+          BottomNavigationBarItem(icon: Icon(Icons.people), title: Text('我的')),
         ],
       ),
     );
