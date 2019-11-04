52e496b7b40cce2f80330e25e126d6cc109b60fd
yanjiepeng <382878591@qq.com>
2019-11-04 17:02:56 +0800

    商品属性参数选择功能

--- myjd/lib/pages/ProductContent/ProductContentFirst.dart
+++ myjd/lib/pages/ProductContent/ProductContentFirst.dart
@@ -20,6 +20,9 @@ class _ProductContentFirstState extends State<ProductContentFirst>
   ProductContentItem _productContent;
 
   List<Attr> _attr = [];
+  String _selectedValue;
+
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
@@ -42,118 +45,104 @@ class _ProductContentFirstState extends State<ProductContentFirst>
   void _initAttr() {
     var attr = this._attr;
 
-    print(_attr);
-
     for (var i = 0; i < attr.length; i++) {
-      print(attr[i].list);
-
       for (var j = 0; j < attr[i].list.length; j++) {
-        //在Attr里创建了checklist存储选中情况
-        attr[i].checkList.add({"title": attr[i].list[j], "checked": false});
+        if (j == 0) {
+          attr[i].checkList.add({"title": attr[i].list[j], "checked": true});
+        } else {
+          attr[i].checkList.add({"title": attr[i].list[j], "checked": false});
+        }
+      }
+    }
+    _getSelectedAttrValue();
+  }
+
+  void _changeAtter(cate, title, setBottomState) {
+    List<Attr> attr = this._attr;
+    for (var i = 0; i < attr.length; i++) {
+      if (attr[i].cate == cate) {
+        for (var j = 0; j < attr[i].list.length; j++) {
+          attr[i].checkList[j]["checked"] = false;
+          if (title == attr[i].checkList[j]['title']) {
+            attr[i].checkList[j]['checked'] = true;
+          }
+        }
       }
     }
 
-    print(attr[0].checkList);
+    setBottomState(() {
+      this._attr = attr;
+    });
+
+    _getSelectedAttrValue();
+  }
+
+  //获取选中的值
+  _getSelectedAttrValue() {
+    var _list = this._attr;
+    List tempArr = [];
+    for (var i = 0; i < _list.length; i++) {
+      for (var j = 0; j < _list[i].checkList.length; j++) {
+        if (_list[i].checkList[j]['checked'] == true) {
+          tempArr.add(_list[i].checkList[j]["title"]);
+        }
+      }
+    }
+    // print(tempArr.join(','));
+    setState(() {
+      this._selectedValue = tempArr.join(',');
+    });
   }
 
   //封装组建渲染商品选择属性
 
-  _attrBottomSheet() {
-    showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return GestureDetector(
-            //解决showModalBottomSheet点击消失
-            onTap: () {
-              return false;
-            },
-
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(ScreenAdapter.width(20)),
-                  child: ListView(
-                    children: <Widget>[
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: this._attr.map((attrItem) {
-                            return Wrap(
-                              children: <Widget>[
-                                Container(
-                                  child: Padding(
-                                    padding: EdgeInsets.only(
-                                        top: ScreenAdapter.height(46)),
-                                    child: Text(
-                                      '${attrItem.cate}',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                  width: ScreenAdapter.width(100),
-                                ),
-                                Container(
-                                  width: ScreenAdapter.width(610),
-                                  child: Wrap(
-                                    children: attrItem.checkList.map((value) {
-                                      return Container(
-                                        margin: EdgeInsets.all(10),
-                                        child: Chip(
-                                          label: Text('${value['title']}'),
-                                          padding: EdgeInsets.all(10),
-                                          backgroundColor: value['checked']
-                                              ? Colors.red
-                                              : Colors.grey,
-                                        ),
-                                      );
-                                    }).toList(),
-                                  ),
-                                )
-                              ],
-                            );
-                          }).toList())
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  width: ScreenAdapter.width(750),
-                  height: ScreenAdapter.height(76),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          child: JdButton(
-                            color: Color.fromRGBO(253, 1, 0, 0.9),
-                            text: "加入购物车",
-                            cb: () {
-                              print('加入购物车');
-                            },
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            child: JdButton(
-                              color: Color.fromRGBO(255, 165, 0, 0.9),
-                              text: "立即购买",
-                              cb: () {
-                                print('立即购买');
-                              },
-                            )),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          );
-        });
+  //循环具体属性
+  List<Widget> _getAttrItemWidget(attrItem, setBottomState) {
+    List<Widget> attrItemList = [];
+    attrItem.checkList.forEach((item) {
+      attrItemList.add(Container(
+        margin: EdgeInsets.all(10),
+        child: InkWell(
+          onTap: () {
+            _changeAtter(attrItem.cate, item["title"], setBottomState);
+          },
+          child: Chip(
+            label: Text("${item["title"]}"),
+            padding: EdgeInsets.all(10),
+            backgroundColor: item["checked"] ? Colors.red : Colors.black26,
+          ),
+        ),
+      ));
+    });
+    return attrItemList;
   }
 
+  //封装一个组件 渲染attr
+  List<Widget> _getAttrWidget(setBottomState) {
+    List<Widget> attrList = [];
+    this._attr.forEach((attrItem) {
+      attrList.add(Wrap(
+        children: <Widget>[
+          Container(
+            width: ScreenAdapter.width(120),
+            child: Padding(
+              padding: EdgeInsets.only(top: ScreenAdapter.height(22)),
+              child: Text("${attrItem.cate}: ",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Container(
+            width: ScreenAdapter.width(590),
+            child: Wrap(
+              children: _getAttrItemWidget(attrItem, setBottomState),
+            ),
+          )
+        ],
+      ));
+    });
+
+    return attrList;
+  }
   @override
   Widget build(BuildContext context) {
     ScreenAdapter.init(context);
@@ -224,21 +213,25 @@ class _ProductContentFirstState extends State<ProductContentFirst>
             ),
           ),
           //筛选
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            height: ScreenAdapter.height(80),
-            child: InkWell(
-              onTap: () {
-                _attrBottomSheet();
-              },
-              child: Row(
-                children: <Widget>[
-                  Text("已选: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text("115，黑色，XL，1件")
-                ],
-              ),
-            ),
-          ),
+          this._attr.length > 0
+              ? Container(
+                  margin: EdgeInsets.only(top: 10),
+                  height: ScreenAdapter.height(80),
+                  child: InkWell(
+                    onTap: () {
+                      _attrBottomSheet();
+                    },
+                    child: Row(
+                      children: <Widget>[
+                        Text("已选: ",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text(
+                            "${this._selectedValue == null ? "未选中" : this._selectedValue}")
+                      ],
+                    ),
+                  ),
+                )
+              : Text(''),
           Divider(),
           Container(
             height: ScreenAdapter.height(80),
@@ -255,7 +248,72 @@ class _ProductContentFirstState extends State<ProductContentFirst>
     ));
   }
 
-  @override
-  // TODO: implement wantKeepAlive
-  bool get wantKeepAlive => true;
+
+  //底部弹出框
+  //底部弹出框
+  _attrBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (contex) {
+          return StatefulBuilder(
+            builder: (BuildContext context, setBottomState) {
+              return GestureDetector(
+                //解决showModalBottomSheet点击消失的问题
+                onTap: () {
+                  return false;
+                },
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(ScreenAdapter.width(20)),
+                      child: ListView(
+                        children: <Widget>[
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: _getAttrWidget(setBottomState))
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      width: ScreenAdapter.width(750),
+                      height: ScreenAdapter.height(76),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: JdButton(
+                                color: Color.fromRGBO(253, 1, 0, 0.9),
+                                text: "加入购物车",
+                                cb: () {
+                                  print('加入购物车');
+                                },
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                child: JdButton(
+                                  color: Color.fromRGBO(255, 165, 0, 0.9),
+                                  text: "立即购买",
+                                  cb: () {
+                                    print('立即购买');
+                                  },
+                                )),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          );
+        });
+  }
+
 }
